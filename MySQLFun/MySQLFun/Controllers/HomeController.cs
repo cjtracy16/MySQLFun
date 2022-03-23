d74fa32ebd88c6cb267c2d424aec764e06349d42
cjtracy <connertracy@comcast.net>
2022-03-23 09:12:54 -0600

    Created App

---
+++ MySQLFun/MySQLFun/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySQLFun.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLFun.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
